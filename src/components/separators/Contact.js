946056b096f92d305cf2e750df8f473e0f015788
Ruben Melendez <ruben@MacBook-Pro.local>
2020-09-13 21:28:33 -0700

    desktop version of contact is basically done besides the content

--- src/components/separators/Contact.js
+++ src/components/separators/Contact.js
@@ -18,7 +18,16 @@ export default class BigBoiSeperator extends React.Component {
                 <div className={'mainBody'}>
                     <div id={'contactSectionLeft'} className={'sideSection'}>
                         <div className={'headerHolder'}>
+                            <span className={'header'}>Get In</span>
                             <span className={'header'}>Contact</span>
+                        </div>
+
+
+                        <div className={'rightSideText'}>
+                            Cliche prism four loko, sustainable selvage raw denim iPhone affogato enamel pin. Man braid
+                            asymmetrical viral, quinoa live-edge jean shorts lo-fi marfa typewriter vegan migas. Iceland
+                            woke fanny pack bicycle rights. Organic kinfolk subway tile, art party portland kombucha
+                            pour-over.
                         </div>
                     </div>
 
@@ -29,25 +38,45 @@ export default class BigBoiSeperator extends React.Component {
                                     type={'text'}
                                     className={'contactFormInput'}
                                     id={'nameInput'}
+                                    placeholder={'Name'}
                                 />
                                 <input
                                     type={'text'}
                                     className={'contactFormInput'}
                                     id={'numberInput'}
+                                    placeholder={'Number'}
                                 />
-                            </div>
-                            <div className={'formSection email'}>
+
                                 <input
                                     type={'text'}
                                     className={'contactFormInput'}
                                     id={'emailInput'}
+                                    placeholder={'Email'}
                                 />
                             </div>
+
+                            <div className={'formSection textarea'}>
+                                      <textarea
+                                          placeholder={'Message'}
+                                      />
+                            </div>
                             <div className={'formSection bottom'}>
-                                <div className={'contactBotSection'}>
-                                    <textarea/>
+                                <div className={'contactBotSection radiosHolder'}>
+                                    <div>
+                                        <input type="radio" id="exterior" name="service" value="exterior"/>
+                                        <label htmlFor="exterior">Exterior</label>
+
+                                    </div>
+                                    <div>
+                                        <input type="radio" id="interior" name="service" value="interior"/>
+                                        <label htmlFor="interior">Interior</label>
+                                    </div>
+                                    <div>
+                                        <input type="radio" id="other" name="service" value="other"/>
+                                        <label htmlFor="other">Other</label>
+                                    </div>
                                 </div>
-                                <div className={'contactBotSection'}>
+                                <div className={'contactBotSection sendHolder'}>
                                     <button>send</button>
                                 </div>
                             </div>
